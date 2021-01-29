1e8f087f84f78d0311cb42534ddba089d5ee4fd8
lucianoNavomi <lucianoNavomi>
2021-01-29 17:28:50 -0300

    Created route to send whatsApp message

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ app.get('/', function (req, res) {
   res.send('Welcome to Twilio API WhatsApp by Luciano Buhler!');
 });
 
+app.get('/sendMessage', function (req, res) {
+  require('dotenv').config()
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require('twilio')(accountSid, authToken);
+
+  const waFrom = process.env.WA_NUMBER_FROM;
+  const waTo = process.env.WA_NUMBER_TO;
+
+  client.messages
+    .create({
+      from: waFrom,
+      body: 'Hello there!',
+      to: waTo
+    })
+    .then(message => {
+      console.log(message.sid)
+      res.status(200).send('Message sent Succesfully!! Message ID: ', message.sid);
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(400).send('Message sent Failure!!');
+    });
+});
+
 app.listen(PORT, function () {
   console.log(`Twilio API WhatsApp app listening on port ${PORT}!`);
 });
